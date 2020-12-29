d6336935579682b8733c2f4213becb5cc35488b1
Maksim <maxim.s.81@gmail.com>
2020-12-29 17:42:48 +0300

    add images

--- VKClient/Friends/FriendsView.swift
+++ VKClient/Friends/FriendsView.swift
@@ -9,18 +9,19 @@ import UIKit
 
 class FriendsView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // Готовим данные для таблицы
-    var friendsData: [userVK] = [(userVK(name: "Vash", sername: "the Stampede", avatar: UIImage(named: "trigunicon")!, photo: [UIImage(named: "trigunimage")!, UIImage(named: "vash1")!, UIImage(named: "vash2")!, UIImage(named: "vash3")!, UIImage(named: "vash4.jpg")!, UIImage(named: "vash5.jpg")!], gender: .male, idGroup: 1, city: "Gunsmoke")),
-                                 (userVK(name: "Alukard", sername: "Lord", avatar: UIImage(named: "alukardicon")!, photo: [UIImage(named: "alukardimage")!, UIImage(named: "alukard1")!, UIImage(named: "alukard2")!, UIImage(named: "alukard3")!, UIImage(named: "alukard4")!], gender: .male, idGroup: 1, city: "Valahia")),
-                                 (userVK(name: "Nicholas D.", sername: "Wolfwood", avatar: UIImage(named: "wolfwoodavatar")!, photo: [UIImage(named: "wolfwood2")!, UIImage(named: "wolfwood3")!], gender: .male , idGroup: 1, city: "Evergreen")),
-                                 (userVK(name: "Knives", sername: "Millions", avatar: UIImage(named: "knivesavatar")!, photo: [UIImage(named: "knives1")!, UIImage(named: "knives2")!, UIImage(named: "knives3")!, UIImage(named: "knives4")!], gender: .male , idGroup: 1, city: "Gunsmoke")),
-                                 (userVK(name: "Meryl", sername: "Stryfe", avatar: UIImage(named: "merylavatar")!, photo: [UIImage(named: "meryl1")!, UIImage(named: "meryl2")!, UIImage(named: "meryl3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
-                                 (userVK(name: "Milly", sername: "Thompson", avatar: UIImage(named: "millyavatar")!, photo: [UIImage(named: "milly1")!, UIImage(named: "milly2")!, UIImage(named: "milly3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
-                                 (userVK(name: "Kuroneko", sername: "- sama", avatar: UIImage(named: "kuronekoavatar")!, photo: [UIImage(named: "kuroneko1")!, UIImage(named: "kuroneko2")!, UIImage(named: "kuroneko3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
-                                 (userVK(name: "Sho", sername: "Cheon", avatar: UIImage(named: "shoavatar")!, photo: [UIImage(named: "sho1")!, UIImage(named: "sho2")!, UIImage(named: "sho3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
-                                 (userVK(name: "Ichigo", sername: "Kurosaki", avatar: UIImage(named: "ichiavatar")!, photo: [UIImage(named: "ichi1")!, UIImage(named: "ichi2")!, UIImage(named: "ichi3")!, UIImage(named: "ichi4")!, UIImage(named: "ichi5")!], gender: .male, idGroup: 1, city: "Karakura"))
-                                 
+    var friendsData: [userVK] = [
+        (userVK(name: "Vash", sername: "the Stampede", avatar: UIImage(named: "trigunicon")!, photo: [UIImage(named: "trigunimage")!, UIImage(named: "vash1")!, UIImage(named: "vash2")!, UIImage(named: "vash3")!, UIImage(named: "vash4.jpg")!, UIImage(named: "vash5.jpg")!], gender: .male, idGroup: 1, city: "Gunsmoke")),
+        (userVK(name: "Alukard", sername: "Lord", avatar: UIImage(named: "alukardicon")!, photo: [UIImage(named: "alukardimage")!, UIImage(named: "alukard1")!, UIImage(named: "alukard2")!, UIImage(named: "alukard3")!, UIImage(named: "alukard4")!], gender: .male, idGroup: 1, city: "Valahia")),
+        (userVK(name: "Nicholas D.", sername: "Wolfwood", avatar: UIImage(named: "wolfwood1")!, photo: [UIImage(named: "wolfwood2")!, UIImage(named: "wolfwood3")!], gender: .male , idGroup: 1, city: "Evergreen")),
+        (userVK(name: "Knives", sername: "Millions", avatar: UIImage(named: "knivesavatar")!, photo: [UIImage(named: "knives1")!, UIImage(named: "knives2")!, UIImage(named: "knives3")!, UIImage(named: "knives4")!], gender: .male , idGroup: 1, city: "Gunsmoke")),
+        (userVK(name: "Meryl", sername: "Stryfe", avatar: UIImage(named: "meryl0")!, photo: [UIImage(named: "meryl1")!, UIImage(named: "meryl2")!, UIImage(named: "meryl3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
+        (userVK(name: "Milly", sername: "Thompson", avatar: UIImage(named: "milly0")!, photo: [UIImage(named: "milly1")!, UIImage(named: "milly2")!, UIImage(named: "milly3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
+        (userVK(name: "Kuroneko", sername: "- sama", avatar: UIImage(named: "kuroneko0")!, photo: [UIImage(named: "kuroneko1")!, UIImage(named: "kuroneko2")!, UIImage(named: "kuroneko3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
+        (userVK(name: "Sho", sername: "Cheon", avatar: UIImage(named: "shoavatar")!, photo: [UIImage(named: "sho1")!, UIImage(named: "sho2")!, UIImage(named: "sho3")!], gender: .female , idGroup: 1, city: "Gunsmoke")),
+        (userVK(name: "Ichigo", sername: "Kurosaki", avatar: UIImage(named: "ichiavatar")!, photo: [UIImage(named: "ichi1")!, UIImage(named: "ichi2")!, UIImage(named: "ichi3")!, UIImage(named: "ichi4")!, UIImage(named: "ichi5")!], gender: .male, idGroup: 1, city: "Karakura"))
     ]
     
+  
     
     @IBOutlet weak var friendsTableView: UITableView!
     
